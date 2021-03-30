5e3b6e6ed702b88edff59f664a0ff96cd3d1d1e2
Daniel Commins <dencee@users.noreply.github.com>
2021-03-29 17:21:45 -0700

    Update Practice.java

--- src/Practice.java
+++ src/Practice.java
@@ -1,36 +1,57 @@
-
+/*
+ *  A 2D array is comparable to a matrix/grid ordered by rows and columns.
+ *  A 2D array that holds 3, 1D arrays with each 1D array having 3 elements
+ *  can be thought of a 3x3 matrix/grid:
+ *                 col0 col1 col2
+ *                  \/   \/   \/
+ *          row 0  { 1,  2,  3 }
+ *          row 1  { 4,  5,  6 }
+ *          row 2  { 7,  8,  9 }
+ *          
+ *  Where row 0 contains the numbers 1, 2, 3.
+ *  and column 0 contains the numbers 1, 4, 7.
+ *  
+ *  Java stores a 2D array in 'row-major order', meaning the row number is
+ *  the first number in the square brackets. The row 0 elements are:
+ *  
+ *  Row 0 = matrix[0][0], matrix[0][1], matrix[0][2]    // 1, 2, 3  
+ * 
+ *  The second number in the square brackets is the column number.
+ *  The column 0 elements are:
+ *  
+ *  Column 0 = matrix[0][0], matrix[1][0], matrix[2][0] // 1, 4, 7
+ */
 public class Practice {
-    
     /*
-     * Declare a 2D array that can accommodate the grid ov values below:
+     * Return a 2D array that can accommodate the grid of values below:
      *          0, 1, 2
      *          3, 4, 5
      *          6, 7, 8
      */
-    public Object[][] test1_2DInit_a() {
+    public Integer[][] test1() {
         
         return null;
     }
     
     /*
-     * Declare a 2D array that can accommodate the grid of values below:
+     * Return a 2D array that can accommodate the grid of values below:
      *          "a", "b", "c", "d",
      *          "e", "f", "g", "e"
      */
-    public Object[][] test1_2DInit_b() {
+    public String[][] test2() {
         
         return null;
     }
     
     /*
-     * Declare a 2D array that can accommodate the grid of values below:
+     * Return a 2D array that can accommodate the grid of values below:
      *          true, false,
      *          false, false,
      *          false, true,
      *          true, true,
      *          true, false
      */
-    public Object[][] test1_2DInit_c() {
+    public Boolean[][] test3() {
         
         return null;
     }
@@ -42,6 +63,8 @@ public class Practice {
      * int[][] grid = new int[2][3];
      * 
      * 
+     * 
+     * Show your teacher the grid you created
      */
     
     /*
@@ -51,13 +74,15 @@ public class Practice {
      * Robot[][] robots = new Robot[4][2];
      * 
      * 
+     * 
+     * Show your teacher the grid you created
      */
     
     /* Given the array of Objects below, return the element that is not null
      *          null, null, null, null
      *          null, null, null, Object
      */
-    public Object test1_2DInit_d() {
+    public Object test4() {
         Object[][] objects = { {null, null, null, null}, {null, null, null, new Object()} }; 
         
         return null;
@@ -67,7 +92,7 @@ public class Practice {
      *          null, null, Object, null
      *          null, null, null, null
      */
-    public Object test1_2DInit_e() {
+    public Object test5() {
         Object[][] objects = { {null, null, new Object(), null}, {null, null, null, null} }; 
         
         return null;
@@ -77,8 +102,8 @@ public class Practice {
      * Write an algorithm to return the sum of all the numbers in the
      * first row (row 0)
      */
-    public Integer test1_2DInit_f() {
-        int[][] nums = { {8, 7, 6}, {5, 4, 3}, {2, 1, 0}};
+    public Integer test6() {
+        int[][] nums = { {8, 7, 6, 5}, {4, 3}, {2, 1, 0} };
         
         return null;
     }
@@ -87,8 +112,65 @@ public class Practice {
      * Write an algorithm to return the sum of all the numbers in the
      * second column (column 1)
      */
-    public Integer test1_2DInit_g() {
-        int[][] nums = { {8, 7, 6}, {5, 4, 3}, {2, 1, 0}};
+    public Integer test7() {
+        int[][] nums = { {8, 7, 6, 5}, {4, 3}, {2, 1, 0} };
+        
+        return null;
+    }
+    
+    /*
+     * Write an algorithm to return the sum of all the numbers in the
+     * specified row
+     */
+    public Integer getSumByRow(int[][] matrix, int row) {
+        
+        return null;
+    }
+    
+    /*
+     * Write an algorithm to return the sum of all the numbers in the
+     * specified column
+     */
+    public Integer getSumByColumn(int[][] matrix, int col) {
+        
+        return null;
+    }
+    
+    /*
+     * Return the value of the element adjacent to the specified row and column
+     * The directions East, West, North, South are relative to the 2D array
+     * matrix/grid. For example in this 3x3 2D array:
+     * 
+     *          [1][2][3]
+     *          [4][5][6]
+     *          [7][8][9]
+     * 
+     * for the 5 in the middle (row = 1, column = 1), then
+     * East neighbor = 6
+     * West neighbor = 4
+     * North neighbor = 2
+     * South neighbor = 8
+     * 
+     * 1. Assume input row and column values are valid indexes in the matrix.
+     * 2. If the neighbor does not exist, i.e. it's outside the grid
+     *    boundaries, return null.
+     */
+    public Integer getEastNeighbor(int[][] matrix, int row, int col) {
+        
+        return null;
+    }
+    
+    public Integer getWestNeighbor(int[][] matrix, int row, int col) {
+        
+        return null;
+    }
+    
+    public Integer getNorthNeighbor(int[][] matrix, int row, int col) {
+        
+        return null;
+    }
+    
+    public Integer getSouthNeighbor(int[][] matrix, int row, int col) {
         
         return null;
     }
